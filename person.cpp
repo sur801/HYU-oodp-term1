c0740cb86467d08b559afefa2aca19e3b7eef30a
2016025832 <galeb716@daum.net>
2017-03-29 01:06:12 +0900

    commit person.cpp

---
+++ person.cpp
@@ -0,0 +1,100 @@
+/*
+ *  This file is person.cpp
+ *
+ *  @author Jeonghoon Lee
+ *  @since  2017-03-26
+ */
+
+
+#include <iostream>
+#include <algorithm>
+#include "person.h"
+
+using namespace std;
+
+
+//bool cmp(Person& a, Person& b) { return a.name > b.name; }
+
+void Person::displayData() {
+    //cout << "name : " << this->name_ << endl; << "number : " << this->number_ << endl << "group : " << this->group_ << endl;
+}
+
+void Person::loadData() {
+    
+}
+
+void Person::saveData() {
+
+}
+
+void Person::delData(string name) {
+    
+    auto it = vector_name_.begin();
+    
+    // auto를 이용해 vector에 접근
+    for (it; it != vector_name_.end(); it++) {
+        if (it->second->name_ == name) {
+            int number = it->second->number_;
+            delData(number);
+            vector_name_.erase(it);
+        }
+    }
+    
+}
+
+void Person::delData(int number) {
+    auto it = map_number_.find(number);
+    
+    if (it == map_number_.end()) {
+        cout << "not found!" << endl;
+    }
+    
+    string name = it->second->name_;
+    
+    delData(name);
+    map_number_.erase(it);
+}
+
+
+void Person::sortData() {
+    sort(vector_name_.begin(), vector_name_.end());
+}
+
+void Person::searchByNumber(int number) {
+    auto it = map_number_.find(number);
+    
+    if ( it == map_number_.end() )
+        cout << "not found" << endl;
+    else {
+        Person *p = it->second;
+        
+        cout << "name : " << p->name_ << endl << "number : " << p->number_ << endl << "group : " << p->group_ << endl;
+    }
+}
+
+
+void Person::searchByName(string name) {
+    
+    int left = 0;
+    int right = vector_name_.size() - 1;
+
+    while(left <= right)
+    {
+        int mid = (right + left) / 2;
+        if(vector_name_[mid].first == name) {
+            Person *p = vector_name_[mid].second;
+            
+            cout << "name : " << p->name_ << endl << "number : " << p->number_ << endl << "group : " << p->group_ << endl;
+            
+        } else if(vector_name_[mid].first < name) {
+            left = mid + 1;
+        } else if(vector_name_[mid].first > name) {
+            right = mid - 1;
+        }
+        
+        
+    }
+    
+    cout << "not found!";
+    
+}
